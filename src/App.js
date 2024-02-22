83a846498252bcc3550638442ba85352cf933728
hneelabh <hneelabh13@gmail.com>
2024-02-22 10:56:44 +0530

    Commit and push

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component, createContext, useState } from 'react';
+import Header from './components/Header';
+import ResultList from './components/ResultList';
+
+export const InputContext = createContext();
+
+function App () {
+  const [inputValue, setInputValue] = useState("");
+
+  const value = {
+    inputValue, setInputValue
+  }
+
+  return (
+    <InputContext.Provider value={value}>
+      <div className="App">
+        <Header />
+        <ResultList/>
+      </div>
+    </InputContext.Provider>
+);
+}
+
+export default App;
